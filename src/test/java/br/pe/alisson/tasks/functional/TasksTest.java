3b433e540e32f315b4f80e75c919c93f782cc85f
zimablu3 <alissonnnunes12@gmail.com>
2023-06-19 08:51:47 -0300

    teste de remocao

--- src/test/java/br/pe/alisson/tasks/functional/TasksTest.java
+++ src/test/java/br/pe/alisson/tasks/functional/TasksTest.java
@@ -140,4 +140,29 @@ public class TasksTest {
 		}
 	}
 	
+	@Test
+	public void deveRemoverTarefaComSucesso() throws MalformedURLException {
+		WebDriver driver = acessarAplicacao();
+		
+		try {
+			//inserir tarefa
+			driver.findElement(By.id("addTodo")).click();
+			driver.findElement(By.id("task")).sendKeys("Teste de lontra2");
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2018");
+			driver.findElement(By.id("saveButton")).click();
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+			
+			//remover a tarefa
+			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
+			message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+			
+		} finally {
+		
+		//fechar o browser
+		driver.quit();
+		}
+	}
+	
 }
